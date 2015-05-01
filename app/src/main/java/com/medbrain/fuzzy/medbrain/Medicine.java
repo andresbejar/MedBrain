c9b693e3ad8bd38de757151b4798633d322c4b9f
Andres Bejarano <andresbejar@gmail.com>
2015-04-30 20:13:43 -0600

    Initial commit

---
+++ app/src/main/java/com/medbrain/fuzzy/medbrain/Medicine.java
@@ -0,0 +1,65 @@
+package com.medbrain.fuzzy.medbrain;
+
+/**
+ * Created by andres on 30/04/15.
+ */
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+
+public class Medicine {
+    private String Name;
+    private int ID;
+    private String Dose;
+    private String Details;
+
+    public Medicine(String name){
+        Name = name;
+    }
+
+    public String getName(){
+        return Name;
+    }
+
+    public int getID(){
+        return ID;
+    }
+
+    public String getDose(){
+        return Dose;
+    }
+
+    public void setDose(String dose) {
+        Dose = dose;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void initializeNewMedicine(){
+        Calendar calendar = new GregorianCalendar();
+
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_YEAR);
+        int hour = calendar.get(Calendar.HOUR);
+        int minute = calendar.get(Calendar.MINUTE);
+        int second = calendar.get(Calendar.SECOND);
+        int ms = calendar.get(Calendar.MILLISECOND);
+
+        int id = (year + month + day + hour + minute + second + ms);
+        this.setID(id);
+    }
+
+    public void setDetails(String _details){
+        Details = _details;
+    }
+
+    public String getDetails(){
+        return Details;
+    }
+
+
+}
---
+++ app/src/main/java/com/medbrain/fuzzy/medbrain/Prescription.java
@@ -0,0 +1,82 @@
+package com.medbrain.fuzzy.medbrain;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+
+public class Prescription {
+    private int ID;
+    private String Doctor; //TODO: Cambiar el String por objeto Contacto
+    private ArrayList<String> Medicines;
+    private Calendar Expiration;
+
+    public Prescription(){
+        Medicines = new ArrayList<String>();
+    }
+
+    public void setID(int _id){
+        ID = _id;
+    }
+
+    public void setDoctor(String doctor) {
+        Doctor = doctor;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getDoctor() {
+        return Doctor;
+    }
+
+    public void setExpiration(Calendar exp){
+        Expiration = exp;
+    }
+
+    public void initializeNewPrescription(){
+        Expiration = new GregorianCalendar();
+        Expiration.setTime(new Date());
+        int year = Expiration.get(Calendar.YEAR);
+        int month = Expiration.get(Calendar.MONTH);
+        int day = Expiration.get(Calendar.DAY_OF_YEAR);
+        int hour = Expiration.get(Calendar.HOUR);
+        int minute = Expiration.get(Calendar.MINUTE);
+        int second = Expiration.get(Calendar.SECOND);
+        int ms = Expiration.get(Calendar.MILLISECOND);
+
+        int id = (year + month + day + hour + minute + second + ms);
+        this.setID(id);
+    }
+
+    public Calendar getExpiration(){
+        return Expiration;
+    }
+
+    public boolean IsEmpty(){
+        return Medicines.isEmpty();
+    }
+
+    public void addMedicine(String _med){
+        Medicines.add(_med);
+    }
+
+    public String getMedicine(int index){
+        return Medicines.get(index);
+    }
+
+    public boolean deleteMedicine(String _med){
+        int size = Medicines.size();
+        for(int i = 0; i < size; i++){
+            if(Medicines.get(i).compareTo(_med) == 0){
+                Medicines.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+}
